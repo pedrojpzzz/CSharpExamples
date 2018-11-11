8892e65da2f2cefcfcd681080db1ca9e6b892700
pedrojpzzz <pedrojpzzz@gmail.com>
2018-11-11 01:43:38 +0100

    Singleton Example

---
+++ CSharpExamples/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpExamples
+{
+    public class Singleton
+    {
+        private Singleton singleton;
+
+        private string mySingletonInfo;
+
+        private Singleton()
+        {
+            mySingletonInfo = "my Singleton Instance";
+        }
+
+        public Singleton GetInstante()
+        {
+            if (singleton.Equals(null))
+            {
+                singleton = new Singleton();
+            }
+
+            return singleton;
+        }
+    }
+}
